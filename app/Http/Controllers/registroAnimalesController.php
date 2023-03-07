d332df49ec3d94b8ed09af4cd72caa9941aea442
Daniel Richard Condori Rodriguez <daniel.condori@xema.tech>
2023-03-07 15:07:04 -0400

    completando y solucionando el error que aparecia al registrar ganado

--- app/Http/Controllers/registroAnimalesController.php
+++ app/Http/Controllers/registroAnimalesController.php
@@ -14,7 +14,7 @@ class registroAnimalesController extends Controller
         $datos = $request->all();
         //dd($datos);
         // Guardar los datos en la base de datos utilizando una consulta SQL
-        DB::insert('INSERT INTO animales (nombre, codigo, sexo, peso, edad, raza, numeroVacunas, numeroCrias) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [$datos['name'], $datos['codigo'],$datos['sexo'],$datos['peso'],$datos['edad'],$datos['raza'],$datos['vacunas'],$datos['crias']]);
+        DB::insert('INSERT INTO animales (nombre, codigo, sexo, peso, edad, raza, numeroVacunas, numeroCrias) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [$datos['nombre'], $datos['codigo'],$datos['sexo'],$datos['peso'],$datos['edad'],$datos['raza'],$datos['numeroVacunas'],$datos['numeroCrias']]);
 
         //$newAnimal = animales::insert('INSERT INTO tu_tabla (nombre, codigo) VALUES (?, ?)', [$datos['nombre'], $datos['codigo']]);
         //$newAnimal = animales::create(request(['nombre','codigo','sexo','peso','edad','raza','numeroVacunas','numeroCrias']));
